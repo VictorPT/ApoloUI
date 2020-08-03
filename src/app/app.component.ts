14985842ae7dc76d01f43f5627dda08bd94b7ef1
VictorPT <victor_13_@msn.com>
2020-08-02 23:36:38 -0400

    Testing http libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,15 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+
+  readonly ROOT_URL = 'http://172.23.112.1:8080/apolo';
+
+  posts: any;
+
+  constructor(private http: HttpClient) {}
+
+  getPosts(){
+    this.posts = this.http.get(this.ROOT_URL + "/userList")
+  }
+
 }
